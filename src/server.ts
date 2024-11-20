cb8fcd0f467d45c9152f5435b9e2d0f85f835465
prestonb91 <prestonb91@gmail.com>
2024-11-20 13:57:27 +0900

    clean draft of endpoints.

--- src/server.ts
+++ src/server.ts
@@ -7,34 +7,29 @@ const {loginHandler, logoutHandler} = require("./auth/handlers");
 app.post("/login", loginHandler);
 app.post("/logout", logoutHandler);
 
-// organization registration
-app.post("/registration", registrationController.register); // add admin account and organization 
-
-// personal account data
-app.get("/accounts/accountId", accountController); // get account data (including special PTO)
-app.patch("/accounts/accountId", accountController); // edit account data through settings
+// registration and organization management
+app.post("/registration", organizationController.register); // add admin account and organization 
+app.patch("/organizations/organizationId", organizationController); // edit organization name
 
 // organization account management
 app.get("/accounts", accountController) // get all accounts to show in employee list
-app.post("/accounts", accountController); // add an account
+app.post("/accounts", accountController); // add an employee account
+app.patch("/accounts/accountId", accountController); // edit account data through settings
 app.delete("/accounts/accountId", accountController); // delete an account
 
-// organization management
-app.patch("/organizations/organizationId", organizationController); // edit organization name
+// attendance logging
+app.get("/accounts/accountId/attendance/", attendanceController); // get all attendance records to populate calendar for one user
+app.post("/accounts/accountId/attendance/attendanceId", attendanceController); // add attendance record
+app.put("/accounts/accountId/attendance/attendanceId", attendanceController); // edit attendance record
 
-// approvals
-// get all approvals related to that accountId through app.get("accountId");
-app.get("/approvals/approverId", approvalController); // get all calendars of users that have set approver as this account
-app.post("/approvals/monthAttendance/approverId", approvalController); // add new approval for monthly attendance
-app.post("/approvals/pto/approverId", approvalController); // add new approval for PTO
-app.post("/approvals/specialPto/approverId", approvalController); // add new approval for special PTO
+// approvals and supervisors 
+app.get("/accounts/accountId/approvals", approvalController); // get all approvals related to that id 
+app.get("/approvals/supervisorId", approvalController); // get all approvals and calendars of users that have set approver as this account
+app.post("/approvals/monthAttendance/supervisorId", approvalController); // add new approval for monthly attendance
+app.post("/approvals/pto/supervisorId", approvalController); // add new approval for PTO
+app.post("/approvals/specialPto/supervisorId", approvalController); // add new approval for special PTO
 app.patch("/approvals/approvalId", approvalController); // changes to an approval item status
 app.delete("/approvals/approvalId", approvalController); // delete approval
-
-// attendance logging
-app.get("/accounts/accountId/attendance/attendanceId", attendanceController); // add attendance record to populate calendar
-app.post("/accounts/accountId/attendance/attendanceId", attendanceController); // add attendance record
-app.patch("/accounts/accountId/attendance/attendanceId", attendanceController); // edit attendance record
 
 // team management and calendar view
 app.get("/teams", teamController); // get list of all teams
@@ -43,6 +38,7 @@ app.patch("/teams/teamId", teamController); // edit a team name
 app.delete("/teams/teamId", teamController); // delete a team
 
 // special PTO data management
+app.get("/accounts/accountId/specialPto", specialPtoController); // view special PTO for that account
 app.post("/accounts/accountId/specialPto", specialPtoController); // add special PTO
 app.patch("/accounts/accountId/specialPto", specialPtoController); // edit special PTO type
 app.delete("/accounts/accountId/specialPto", specialPtoController); // delete special PTO
