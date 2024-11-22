0fb9c76296dae004c16ec377fd92788be5580cef
prestonb91 <prestonb91@gmail.com>
2024-11-22 19:14:49 +0900

    feat: functionality to add admin and org on initial registration.

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,17 +1,39 @@
 import organizationModel from "../models/organization.model";
 
 // add admin account and organization
-// need to confirm how admin registration works on backend
 const initialRegistration = async (req : any, res : any) => {
     try {
-        console.log("initialRegistration received");
-        const orgName = req.body.orgName;
-        const adminData = req.body
-        console.log(req.body.orgName);
-        console.log(req.body.adminData);
-        
-        // const registerAdmin = await organizationModel.addAdmin(adminData);
-        // const registerOrg = await organizationModel.addOrg(orgName);
+        const adminData = req.body.adminData;
+
+        // temporary testing, requiring unique auth key 
+        // DELETE AFTER UPDATING SCHEMA
+        function makeid(length : any) {
+            let result = '';
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < length) {
+              result += characters.charAt(Math.floor(Math.random() * charactersLength));
+              counter += 1;
+            }
+            return result;
+        }
+        adminData.auth_key = makeid(5);
+
+        // add company and return company id
+        const registerOrgId = await organizationModel.addOrg(adminData.organizationName);
+
+        // add company id into adminData 
+        adminData.organizationId = registerOrgId.id;
+
+        // convert birthdate and join date to iso string
+        adminData.adminDateOfBirth = new Date(adminData.adminDateOfBirth);
+        adminData.adminJoinDate = new Date(adminData.adminJoinDate);
+
+        // add company Id to admin that was just created
+        const registerAdmin = await organizationModel.addAdmin(adminData);
+        console.log("Registration and Org Created", registerAdmin); 
+
     } catch(err) {
         console.error(err);
     };
--- src/models/organization.model.ts
+++ src/models/organization.model.ts
@@ -4,21 +4,32 @@ const prisma = new PrismaClient();
 class Organization {
     constructor() {}
 
-    // static addAdmin(adminData) {
-    //     return prisma.account.create({
-    //         data: {
-
-    //         }
-    //     })
-    // }
-
     static addOrg(orgName : any) {
         return prisma.company.create({
             data: {
                 name: orgName,
+            },
+            select: {
+                id: true,
+            }
+        })
+    }    
+
+    static addAdmin(adminData : any) {
+        return prisma.account.create({
+            data: {
+                email: adminData.adminEmail,
+                auth_key: adminData.auth_key,
+                first_name: adminData.adminFirstName,
+                last_name: adminData.adminLastName,
+                birthdate: adminData.adminDateOfBirth,
+                role: adminData.adminRole,
+                join_date: adminData.adminJoinDate,
+                company_id: adminData.organizationId,
             }
         })
     }
+
 
     // static editOrg(orgEditData) {
     //     return prisma.company.update({
